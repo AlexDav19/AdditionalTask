58c4f6fed3ec3a2a73b23b8c9bd1b2d73436441a
aleks <aleksei-davletov@mail.ru>
2023-06-22 19:03:29 +0300

    Выполнил задания по условным операторам

--- src/Main.java
+++ src/Main.java
@@ -31,17 +31,51 @@ public class Main {
         System.out.println("Задание 3");
         int a = 456;
         int b = (a % 100) / 10;
-        System.out.printf("a = %d%nb = %d%n",a, b);
+        System.out.printf("a = %d%nb = %d%n", a, b);
     }
 
     public static void task4() {
         System.out.println("Задание 4");
-
+        int age = 19;
+        int salary = 58_000;
+        double limit;
+        if (age >= 23) {
+            limit = salary * 3;
+        } else {
+            limit = salary * 2;
+        }
+        if (salary >= 50_000) {
+            limit *= 1.2;
+        } else if (salary >= 80_000) {
+            limit *= 1.5;
+        }
+        System.out.printf("Мы готовы выдать вам кредитную карту с лимитом %.2f рублей%n", limit);
     }
 
     public static void task5() {
         System.out.println("Задание 5");
-
+        int age = 25;
+        int salary = 60_000;
+        int wantedSum = 330_000; // желаемая сумма кредита
+        double interestRate = 10; // процентная ставка
+        double maxPayment = salary / 2; // максимально допустимый платеж
+        double payment; // платеж по кредиту
+        if (age < 23) {
+            interestRate++;
+        } else if (age < 30) {
+            interestRate += 0.5;
+        }
+        if (salary > 80_000) {
+            interestRate -= 0.7;
+        }
+        payment = wantedSum / 100d * interestRate;
+        if (payment < maxPayment) {
+            System.out.printf("Максимальный платеж при ЗП %d равен %.2f рублей. Платеж по кредиту %.2f рублей." +
+                    " Одобрено%n", salary, maxPayment, payment);
+        } else {
+            System.out.printf("Максимальный платеж при ЗП %d равен %.2f рублей. Платеж по кредиту %.2f рублей." +
+                    " Отказано%n", salary, maxPayment, payment);
+        }
     }
 
     public static void task6() {
